0b84ccaf96469caeb4e5c19ea11fa734ac6624f8
luis <empluis@hotmail.com>
2025-01-02 03:38:45 +0000

    buscado bug en boton detalle

--- backend/rma/views.py
+++ backend/rma/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-import rest_framework
-from rest_framework import viewsets
-from .serializers import ClienteSerializer, ProductoSerializer, ReparacionSerializer
-from .models import Cliente, Producto, Reparacion
 from django.db.models import Q
+from rest_framework import viewsets
 from rest_framework.response import Response
+import rest_framework
+from .models import Cliente, Producto, Reparacion
+from .serializers import ClienteSerializer, ProductoSerializer, ReparacionSerializer
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    renderer_classes = [rest_framework.renderers.JSONRenderer]  
+    renderer_classes = [rest_framework.renderers.JSONRenderer]
 
     def list(self, request):
         queryset = self.get_queryset()
@@ -18,24 +17,54 @@ class ClienteViewSet(viewsets.ModelViewSet):
         telefono = request.GET.get('telefono')
         producto = request.GET.get('producto')
         fecha_ingreso = request.GET.get('fecha_ingreso')
+        estado_reparacion = request.GET.get('estado_reparacion')
 
-        q_filter = Q()
+        condiciones = []
 
         if nombre:
-            q_filter &= Q(nombre_completo__icontains=nombre)
-            queryset = queryset.filter(q_filter)  # Aplica el filtro solo si se proporciona un nombre
-            print(q_filter)
+            condiciones.append(Q(nombre_completo__icontains=nombre))
         if telefono:
-            q_filter &= Q(telefono__icontains=telefono)
+            condiciones.append(Q(telefono__icontains=telefono))
         if producto:
-            q_filter &= Q(producto_id=producto)
+            condiciones.append(Q(producto_id=producto))
         if fecha_ingreso:
-            q_filter &= Q(fecha_ingreso__date=fecha_ingreso)
+            condiciones.append(Q(fecha_ingreso__date=fecha_ingreso))
 
-        #queryset = queryset.filter(q_filter)  # Esta línea estaba mal indentada
+        # Combina las condiciones con AND
+        if condiciones:
+            q_filter = Q()
+            for condicion in condiciones:
+                q_filter &= condicion
+            queryset = queryset.filter(q_filter)
+
+        if estado_reparacion:
+            queryset = queryset.filter(reparacion__estado=estado_reparacion)
+
+        queryset = queryset.distinct()
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        cliente = self.get_object()
+        serializer = ClienteSerializer(cliente)
+
+        # Obtener la reparación del cliente
+        try:
+            reparacion = Reparacion.objects.get(cliente=cliente)
+        except Reparacion.DoesNotExist:
+            reparacion = None
+
+        # Incluir la información de la reparación en la respuesta, 
+        # incluyendo campos adicionales si es necesario
+        serializer.data['reparacion'] = {
+            'id': reparacion.id if reparacion else None,
+            'estado': reparacion.estado if reparacion else None,
+            # ... otros campos de la reparación ... 
+        }
+
+        return Response(serializer.data)
+
 
     def perform_create(self, serializer):
         cliente = serializer.save()
@@ -48,15 +77,14 @@ class ClienteViewSet(viewsets.ModelViewSet):
             cliente.foto_producto.name = f'fotos_productos/{cliente.foto_producto.name}'
             cliente.save()
 
+
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    renderer_classes = [rest_framework.renderers.JSONRenderer]  
+    renderer_classes = [rest_framework.renderers.JSONRenderer]
+
 
 class ReparacionViewSet(viewsets.ModelViewSet):
     queryset = Reparacion.objects.all()
     serializer_class = ReparacionSerializer
     renderer_classes = [rest_framework.renderers.JSONRenderer]
-
-
-    
